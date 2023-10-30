1a8537c47e27efa730b31fc66f06e24213b334a4
PyCreatorr <ttptv@yahoo.com>
2023-10-30 23:20:32 +0100

    webhook : internal error response

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -32,11 +32,14 @@ class WebhooksController < ApplicationController
             #@product.increment!(:sales_count)
           
             session_with_expand = Stripe::Checkout::Session.retrieve({ id: session.id, expand: ["line_items"]})
-            puts 'Session with expand' + session_with_expand
-            session_with_expand.line_items.data.each do |line_item|
-              product = Product.find_by(stripe_product_id: line_item.price.product)
-              product.increment!(:sales_count)
-            end
+            p session_with_expand
+            # session_with_expand.line_items.data.each do |line_item|
+            #   product = Product.find_by(stripe_product_id: line_item.price.product)
+            #   product.increment!(:sales_count)
+            # end
+            @product = Product.find_by("stripe_product_id": session.metadata.product_id)
+            p session.metadata.product_id
+            @product.increment!(:sales_count)
           
       end
           
@@ -54,5 +57,4 @@ class WebhooksController < ApplicationController
       
     end
 end
-
 
