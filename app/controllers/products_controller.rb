00283c445be4650707c1305a4b7b1b8d9fecbee8
PyCreatorr <ttptv@yahoo.com>
2023-10-30 09:18:52 +0100

    add_stripe_product_id_to_products+ add currency field

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,7 +22,6 @@ class ProductsController < ApplicationController
   # POST /products or /products.json
   def create
     @product = Product.new(product_params)
-
     respond_to do |format|
       if @product.save
         format.html { redirect_to product_url(@product), notice: "Product was successfully created." }
@@ -65,6 +64,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :price)
+      params.require(:product).permit(:name, :price, :currency)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,22 @@
 class Product < ApplicationRecord
 
-    validates :name, :price, presence: true
+    validates :name, :price, :currency, presence: true
     
     # to call just the product, without name
     def to_s
         name
     end
+
+    after_create do
+        product = Stripe::Product.create(name: name)
+
+        Stripe::Price.create({
+            unit_amount: price,
+            currency: currency,
+            # recurring: {interval: 'month'},
+            product: product.id,
+          })
+        
+        update(stripe_product_id: product.id)
+      end
 end
